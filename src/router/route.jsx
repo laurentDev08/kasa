12a2c6e6c4727f09ce8b8bf5b24b06f15e66b699
doggybroly <laurentdoggy@gmail.com>
2022-10-09 09:28:36 +0200

    organisation

---
+++ src/router/route.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router,Route, Switch } from "react-router-dom";
+const Routeur = () => {
+    return (
+       
+        <h2 className="titre">La Route</h2>
+    );
+}
+ 
+export default Routeur;
