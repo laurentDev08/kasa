c7548e6670bdafd78a39f86d7c64d5cfd935520f
doggybroly <laurentdoggy@gmail.com>
2022-10-22 17:01:04 +0200

    carrousel

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -1,10 +1,28 @@
 import "../style/slideshow.css"
+import { useState } from "react";
 const Slideshow = ( {cover,gallery} ) => {
+    const [index , setIndex] = useState(-1);
+    function next(){
+        let i = index;
+        i++;
+        if(i == gallery.length ){
+            i=0;
+        }
+        setIndex(i)
+    }
+    function prev(){
+        let i = index;
+        i--;
+        if(i < 0  ){
+            i=gallery.length -1;
+        }
+        setIndex(i)
+    }
     return (
         <div className="slideshow">
-            <button className="prev"><i className="fa-solid fa-chevron-left"></i></button>
-            <img src={cover} alt="" />
-            <button className="next"><i className="fa-solid fa-chevron-right"></i></button>
+            <button className="prev" onClick={prev}><i className="fa-solid fa-chevron-left"></i></button>
+            <img src={index <0 ? cover:gallery[index]} alt="" />
+            <button className="next" onClick={next}><i className="fa-solid fa-chevron-right"></i></button>
         </div>
     );
 }
